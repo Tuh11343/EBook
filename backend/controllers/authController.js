256fac84726e560400bc6ed4e2a566a8f1bb5f47
Nicole On <nicole.on.goldenowl@gmail.com>
2024-01-24 15:49:00 +0700

    complete login and sign up route

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,37 +3,53 @@ const AppError = require('../utils/appError');
 const prisma = require('../prisma/prisma');
 const authUtils = require('../utils/authUtils');
 
-
 exports.register = catchAsync(async (req, res, next) => {
-    const data = req.body;
-    try{
-        let account = await prisma.account.findFirst({
-            where: {
-              email: data.email,
-            },
-        });
-        if(account){
-            res.status(400).json({ message: 'Account already exists' });
-            return;
-        }
-      
-        const hashPassword = authUtils().hashPassword(data.password)
-
-        let user = await prisma.user.create({ data: {
-            name: data.name,
-            address: data.address,
-            phoneNumber: data.phoneNumber
-          } });
-      
-          account = await prisma.account.create({
-            data: {            
-                email: data.email,
-                password: hashPassword,
-                user_id: user.id,
-            }
-          });
-          res.status(200).json(user);
-    }catch (e) {
-        next(new AppError(e, 500))
+  const data = req.body;
+  try {
+    let account = await new authUtils().getUserByEmail(data.email);
+    if (account) {
+      res.status(400).json({ message: 'Account already exists' });
+      return;
     }
+
+    const hashPassword = await new authUtils().hashPassword(data.password);
+
+    let user = await prisma.user.create({
+      data: {
+        name: data.name,
+        address: data.address,
+        phoneNumber: data.phoneNumber,
+      },
+    });
+
+    account = await prisma.account.create({
+      data: {
+        email: data.email,
+        password: hashPassword,
+        user_id: user.id,
+      },
+    });
+    res.status(200).json({ message: 'Register successfully!' });
+  } catch (e) {
+    next(new AppError(e, 500));
+  }
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const data = req.body;
+  try {
+    let account = await new authUtils().getUserByEmail(data.email);
+    if (!account) {
+      res.status(400).json({ message: 'Account not exists, please create one!' });
+      return;
+    }
+    const isPasswordValid = await new authUtils().comparePassword(data.password, account.password);
+    if (!isPasswordValid) {
+      res.status(400).json({ message: 'Password is not correct!' });
+      return;
+    }
+    res.status(200).json({ message: 'Login successfully!' });
+  } catch (e) {
+    next(new AppError(e, 500));
+  }
 });
--- backend/utils/authUtils.js
+++ backend/utils/authUtils.js
@@ -1,11 +1,23 @@
+const prisma = require('../prisma/prisma');
 const bcrypt = require('bcrypt');
 const SALT_ROUNDS = 10;
 
-module.exports = class AuthUtils{
-    constructor() {
-    }
-    
-    hashPassword(password) {
-        return bcrypt.hashSync(password, SALT_ROUNDS);
-    }
-}
+module.exports = class AuthUtils {
+  constructor() {}
+
+  getUserByEmail(email) {
+    return prisma.account.findFirst({
+      where: {
+        email: email,
+      },
+    });
+  }
+
+  hashPassword(password) {
+    return bcrypt.hashSync(password, SALT_ROUNDS);
+  }
+
+  comparePassword(password, hash) {
+    return bcrypt.compareSync(password, hash);
+  }
+};
