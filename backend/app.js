637e6b81776419c4b41826118f624714011d5e0a
Nicole On <nicole.on.goldenowl@gmail.com>
2024-01-24 15:34:04 +0700

    update express json

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,24 @@
 const express = require('express');
-
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
 
 const port = process.env.PORT || 5000;
 
 const userRoutes = require('./routes/userRoute');
+const authRoutes = require('./routes/authRoute');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
+app.use(express.json({ limit: '10mb' }));
+app.use(xss());
+app.use(hpp());
 app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/auth', authRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
