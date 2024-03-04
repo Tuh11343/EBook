66721cfee4457847d20f40da7c58417b92854f2d
Tuh <dotuan2k2@gmail.com>
2024-03-04 07:48:49 +0700

    Changing type return genre api service

--- app/src/main/java/com/example/ebook/api/GenreAPIService.kt
+++ app/src/main/java/com/example/ebook/api/GenreAPIService.kt
@@ -1,16 +1,19 @@
 package com.example.ebook.api
 
-import com.example.ebook.model.GenreResponse
-import com.example.ebook.model.GenresResponse
+import com.google.gson.JsonElement
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Path
-
+import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface GenreAPIService {
-    @GET("/genre/id/{id}")
-    fun findByID(@Path("id") id:Int): Call<GenreResponse>
+    @GET("/genre/id")
+    fun findByID(@Query("id") id:Int):Call<JsonElement>
 
     @GET("/genre")
-    fun findAll():Call<GenresResponse>
+    fun findAll(@Query("limit") limit:Int, @Query("offset") offset:Int):Call<JsonElement>
+
+    @POST("/payment-sheet")
+    fun payment():Call<JsonElement>
+
 }
--- app/src/main/java/com/example/ebook/views/MainActivity.kt
+++ app/src/main/java/com/example/ebook/views/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.ebook.views
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ebook.adapter.GenreAdapter
 import com.example.ebook.api.GenreAPIService
 import com.example.ebook.databinding.MainBinding
 import com.example.ebook.model.Genre
-import com.example.ebook.model.GenresResponse
+import com.example.ebook.utils.PaginationScrollListener
 import com.example.ebook.utils.RetrofitClient
+import com.google.gson.JsonElement
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -27,30 +31,89 @@ class MainActivity : ComponentActivity() {
         binding = MainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        getGenres()
+
+    }
+
+    private fun getGenres() {
         val apiService: GenreAPIService = RetrofitClient.get()!!.create(GenreAPIService::class.java)
-        val call: Call<GenresResponse> = apiService.findAll()
-        call.enqueue(object : Callback<GenresResponse> {
-            override fun onResponse(call: Call<GenresResponse>, response: Response<GenresResponse>) {
+
+        val call: Call<JsonElement> = apiService.findAll(12, 0)
+        call.enqueue(object : Callback<JsonElement> {
+            override fun onResponse(
+                call: Call<JsonElement>,
+                response: Response<JsonElement>
+            ) {
                 if (response.isSuccessful) {
                     try {
-                        genreAdapter=GenreAdapter(response.body()!!.genres)
+                        var jsonElement=response.body()!!
+
+                        genreAdapter =
+                            GenreAdapter(Genre.getGenreList(jsonElement),Genre.getLength(jsonElement))
                         binding.genreList.layoutManager =
-                            LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
-                        binding.genreList.adapter=genreAdapter
+                            LinearLayoutManager(
+                                this@MainActivity,
+                                LinearLayoutManager.VERTICAL,
+                                false
+                            )
+                        binding.genreList.adapter = genreAdapter
+
+                        binding.genreList.addOnScrollListener(object: PaginationScrollListener(binding.genreList.layoutManager as LinearLayoutManager){
+                            override fun loadMoreItem() {
+                                Toast.makeText(this@MainActivity,"Load more item", Toast.LENGTH_SHORT).show()
+                                genreAdapter.addFooterLoading()
+                                val call: Call<JsonElement> = apiService.findAll(2,genreAdapter.genreList.size-1)
+                                call.enqueue(object : Callback<JsonElement> {
+                                    override fun onResponse(
+                                        call: Call<JsonElement>,
+                                        response: Response<JsonElement>
+                                    ) {
+                                        val handler = Handler(Looper.getMainLooper())
+
+                                        val runnable = Runnable {
+                                            genreAdapter.removeFooterLoading()
+                                            genreAdapter.genreList.addAll(Genre.getGenreList(response.body()!!))
+                                            genreAdapter.updateUI()
+                                        }
+
+                                        handler.postDelayed(runnable, 2000)
+                                    }
+
+                                    override fun onFailure(
+                                        call: Call<JsonElement>,
+                                        t: Throwable
+                                    ) {
+                                        Log.e("ERROR", "Error:${t}")
+                                        genreAdapter.removeFooterLoading()
+                                    }
+
+                                })
+                            }
+
+                            override fun isLoading(): Boolean {
+                                return genreAdapter.isLoadingAdd
+                            }
+
+                            override fun isLastPage(): Boolean {
+                                return genreAdapter.isLastPage()
+                            }
+
+                        })
 
                     } catch (e: IOException) {
                         e.printStackTrace()
                     }
                 } else {
-                    Log.i("Nothing","Du lieu rong:"+response.body())
+                    Log.i("Nothing", "Du lieu rong:" + response.body())
                 }
             }
 
-            override fun onFailure(call: Call<GenresResponse>, t: Throwable) {
+            override fun onFailure(call: Call<JsonElement>, t: Throwable) {
                 t.printStackTrace()
-                Log.e("ERROR","Loi:${t.toString()}")
+                Log.e("ERROR", "Loi:${t}")
             }
         })
+
 
     }
 }
