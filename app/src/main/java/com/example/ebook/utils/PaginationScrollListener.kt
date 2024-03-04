b26203abd464208aea6250bdf4355d1696478b42
Tuh <dotuan2k2@gmail.com>
2024-03-04 07:49:49 +0700

    Add Pagination for Genre Adapter

---
+++ app/src/main/java/com/example/ebook/utils/PaginationScrollListener.kt
@@ -0,0 +1,28 @@
+package com.example.ebook.utils
+
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class PaginationScrollListener(var linearLayoutManager: LinearLayoutManager): RecyclerView.OnScrollListener() {
+
+    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+        super.onScrolled(recyclerView, dx, dy)
+
+        val totalItemCount = linearLayoutManager.itemCount
+        val visibleItemCount = linearLayoutManager.childCount
+        val firstVisibleItemPosition = linearLayoutManager.findFirstVisibleItemPosition()
+
+        if (firstVisibleItemPosition > 0 && (visibleItemCount + firstVisibleItemPosition) >= totalItemCount) {
+            if(isLoading()||isLastPage()){
+                return
+            }else{
+                loadMoreItem()
+            }
+        }
+    }
+
+    abstract fun loadMoreItem()
+    abstract fun isLoading():Boolean
+    abstract fun isLastPage():Boolean
+    
+}
