1afbb15e11189a0d57ec4a6e92990c41dce3da05
Tuh <dotuan2k2@gmail.com>
2024-03-04 07:56:59 +0700

    Update build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -74,6 +75,9 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     // For control over item selection of both touch and mouse driven selection
     implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
+
+    implementation("com.stripe:stripe-android:20.37.4")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 
 
 }
