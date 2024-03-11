cbc59ab418310a2667e2d2ea32deec886ce41dac
Hao Vinh <phamhaovinh2002@gmail.com>
2024-03-11 08:38:16 +0700

    Push file menu va array

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -75,5 +77,8 @@ dependencies {
     // For control over item selection of both touch and mouse driven selection
     implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
 
+    implementation("com.stripe:stripe-android:20.37.4")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation ("androidx.cardview:cardview:1.0.0")
 
 }
