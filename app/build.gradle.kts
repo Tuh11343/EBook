f060450ae26957d3d61cd176007d6ca6ed5b7022
Tuh <dotuan2k2@gmail.com>
2024-03-14 13:54:48 +0700

    Update GenreViewModel adding handle error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -90,5 +92,6 @@ dependencies {
     // (see https://github.com/ReactiveX/RxJava/releases for latest 3.x.x version)
     implementation("io.reactivex.rxjava3:rxjava:3.1.5")
     implementation("com.github.akarnokd:rxjava3-retrofit-adapter:3.0.0")
+    implementation("com.tbuonomo:dotsindicator:5.0")
 
 }
