8bd91e90d11e7d35210e66e798a6e4806fc1d630
Tuh <dotuan2k2@gmail.com>
2024-03-21 14:19:31 +0700

    Add app instance

---
+++ app/src/main/java/com/example/ebook/utils/AppInstance.kt
@@ -0,0 +1,13 @@
+package com.example.ebook.utils
+
+
+class AppInstance {
+
+    companion object{
+        var REWIND_REQUEST_CODE=0
+        var FF_REQUEST_CODE=1
+        var PLAY_PAUSE_REQUEST_CODE=2
+        var CLOSE_REQUEST_CODE=3
+    }
+
+}
